9e3ea52803bb89933ee6f7625dac16078fb0131f
Tim Davies <tim.davies@opendataservices.coop>
2017-08-30 16:46:44 +0100

    Fixing scripts to reset path on exit

--- real-examples/tools/process_record.sh
+++ real-examples/tools/process_record.sh
@@ -7,3 +7,5 @@ python convert_releases_to_bigquery_schema.py -f ../$1/all/records/ | ts '[%Y-%m
 
 bq rm --project_id ocds-172716 new_records.$2  | ts '[%Y-%m-%d %H:%M:%S]' | tee -a logs/$logfile.log
 bq load --max_bad_records=100 --source_format=NEWLINE_DELIMITED_JSON --schema=release-schema-bq.json --project_id ocds-172716 new_records.$2 staging/$1/all/records/all.json | ts '[%Y-%m-%d %H:%M:%S]' | tee -a logs/$logfile.log
+
+../
